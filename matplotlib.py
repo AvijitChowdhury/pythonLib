39a071e1df71f7fdf279b2cb7520e3e791acc7c1
Avijit Chowdhury <abhichy30@gmail.com>
2022-07-03 01:10:53 +0600

    matplot

---
+++ matplotlib.py
@@ -0,0 +1,18 @@
+
+
+# import numpy as np
+
+# import matplotlib.pyplot as plt
+
+# x = np.linspace(1,10,10)
+
+# y = x**x
+
+# plt.plot(x,y)
+# plt.show()
+
+import matplotlib.pyplot as plt
+
+plt.plot([1, 2, 3, 4], [1, 4, 9, 16]) 
+plt.axis([0, 6, 0, 20]) 
+plt.show()
